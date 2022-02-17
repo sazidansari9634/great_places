6113c9705394a240544a66c9ca161c98a4d76a4e
sazidansari9634 <sazidansari9634@gmail.com>
2022-02-18 03:48:43 +0530

    296. Adding the "Add Place" Screen & An Image Input

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/image_input.dart';
 
 class AddPlaceScreen extends StatefulWidget {
   static const routeName = '/add-place';
@@ -8,20 +9,45 @@ class AddPlaceScreen extends StatefulWidget {
 }
 
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  final _titleController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Add a New Place'),
       ),
-      body: Column(children: <Widget>[
-        Text('User Inputs..'),
-        RaisedButton.icon(
-          icon: Icon(Icons.add),
-          label: Text('Add Place'),
-          onPressed: () {},
-        ),
-      ]),
+      body: Column(
+          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    children: <Widget>[
+                      TextField(
+                        decoration: InputDecoration(labelText: 'Title'),
+                        controller: _titleController,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      ImageInput(),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            RaisedButton.icon(
+              icon: Icon(Icons.add),
+              label: Text('Add Place'),
+              onPressed: () {},
+              elevation: 0,
+              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              color: Theme.of(context).accentColor,
+            ),
+          ]),
     );
   }
 }
